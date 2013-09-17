5417e8933d65fcc7c4a074d659a998d6f4e679bc
neoxu <neoxuisme@gmail.com>
2013-09-17 09:23:33 +0800

    Update season

--- controllers/database.js
+++ controllers/database.js
@@ -6,6 +6,7 @@ var format = require('util').format;
 var dbUrl = format('');
 var userCollection = 'user';
 var clubCollection = 'club';
+var seasonCollection = 'season';
 var courtCollection = 'court';
 
 function dbUpdate(collectionName, query, doc, callback) {	
@@ -103,25 +104,39 @@ function doRemove(collectionName, query, res) {
 
 function fblogin(req, res) {	
 	var urlData = url.parse(req.url,true);
-	//console.log('fblogin: ' + util.inspect(urlData, true));	
+	//console.log('fblogin: ' + util.inspect(req.session, true));	
 	var user = {uid: urlData.query.uid, name: urlData.query.name};
-	req.session.user = user;
-	res.redirect('/');		
-		
-	var query = {
-		uid : urlData.query.uid
-	};
-	var newUser = {
-		$set : {
-			uid : urlData.query.uid,
-			name : urlData.query.name
-		}
-	};
+	req.session.controller = 'fblogin';
+	req.session.user = user;	
 
-	dbUpdate(userCollection, query, newUser); 
+	var query = {uid : urlData.query.uid};
+	var fields = {_id : 0};
+    
+	dbfindOne(userCollection, query, fields, function(err, auser) {
+		if (!err) {
+			if (auser) {
+				req.session.user = auser;
+			} else {
+				var newUser = {
+					$set : {
+						uid : urlData.query.uid,
+						name : urlData.query.name,
+						email : urlData.query.email,
+						birthday : urlData.query.birthday,
+						location : urlData.query.location
+					}
+				};
+				dbUpdate(userCollection, query, newUser);
+			}
+		} else
+			console.log(err);
+			
+		res.redirect('/');	
+	});
 }
 
 function fblogout(req, res) {	
+	req.session.user = {};
 	req.session.user = undefined;
 	res.redirect('/');
 }
@@ -140,46 +155,52 @@ function basketballclub(req, res) {
 	});	
 }
 
-function createclub(req, res) {	  
-	if (req.body.clubName) {		
-		var query = {clubName : req.body.clubName};
-		var club = {clubName: req.body.clubName, clubInfo: req.body.clubInfo};	
-		
-		if (req.files.file) {
-			cloudinary.uploader.upload(req.files.file.path, function(result) {
-				club['backgroundimg'] = result; 
+function createClub(req, res) {	  	
+	if (req.session.user.email && req.session.user.authority && req.session.user.authority > 1) {
+		if (req.body.clubName) {
+			var query = {clubName : req.body.clubName};
+			var club = {
+				owner : req.session.user.email,
+				clubName : req.body.clubName,
+				clubInfo : req.body.clubInfo
+			};
+
+			if (req.files.file) {
+				cloudinary.uploader.upload(req.files.file.path, function(result) {
+					club['backgroundimg'] = result;
+					dbUpdate(clubCollection, query, club, updateCallback);
+				});
+			} else 
 				dbUpdate(clubCollection, query, club, updateCallback);
-			});
-		}
-		else {
-			dbUpdate(clubCollection, query, club, updateCallback);	
-		}
-		
-		function updateCallback(err) {
-			res.redirect('/');
-		}
-	} else {
-		res.render('createclub', {
-			title : 'Basketball Club',
-			layout : 'layout',
-			err : 'Infomation error.',
-			req : req
-		}); 
-	}
+
+			function updateCallback(err) {
+				res.redirect('/');
+			}
+		} else 
+			res.send('Please enter club name.');
+	} else
+		res.send('No authority.');
 }
 
 function club(req, res) {	
 	var urlData = url.parse(req.url,true);
 	if (urlData && urlData.query && urlData.query.clubname) {		
 		var query = {clubName : urlData.query.clubname};
-		var fields = {_id : 0};
+		var fields = {_id : 0};		
+		
 		dbfindOne(clubCollection, query, fields, function(err, club) {
 			if (club) {
+				var canedit = false;
+				if (club.owner && req.session.user && req.session.user.email && 
+					club.owner == req.session.user.email)
+					canedit = true;
+			
 				res.render('club', {
 					title : 'Club Information',
 					layout : 'layout',
 					club : club,
-					req : req
+					req : req,
+					canedit : canedit					
 				});
 			} else
 				res.send(404);
@@ -187,4 +208,50 @@ function club(req, res) {
 	} else {
 		res.redirect('/');
 	} 	
+}
+
+
+function createSeason(req, res) {
+	if (req.session.user.email && req.session.user.authority && req.session.user.authority > 1) {
+		if (req.body.seasonName) {
+			if (req.body.clubName) {
+				var query = {
+					clubName : req.body.clubName,
+					owner : req.session.user.email
+				};
+				
+				var fields = {
+					clubInfo : 0,
+					backgroundimg : 0
+				};
+				
+				dbfindOne(clubCollection, query, fields, function(err, club) {
+
+					if (club) {
+						var query = {
+							clubID : club._id,
+							seasonName : req.body.seasonName
+						};
+						
+						var season = {
+							clubID : club._id,
+							seasonName : req.body.clubName
+						};
+
+						dbUpdate(seasonCollection, query, season, updateCallback);
+
+						function updateCallback(err) {
+							res.redirect('/');
+						}
+
+					} else
+						res.send('Club not found.');
+				});
+
+			} else
+				res.send('No club name');
+		} else
+			res.send('Please enter season name.');
+	} else
+		res.send('No authority');
 }
--- public/assets/js/facebook.js
+++ public/assets/js/facebook.js
@@ -13,25 +13,41 @@ window.fbAsyncInit = function() {
 		oauth : true
 	});	
 };
+
+(function() 
+{
+    var e = document.createElement('script');
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+    e.async = true;
+    document.getElementById('fb-root').appendChild(e);
+}());
    
 //Onclick for fb login
 $('#fblogin').click(function(e) {
 	/*var loginUrl="http://www.facebook.com/dialog/oauth/?"+
                  "scope=publish_stream&"+
                  "client_id="+APP_ID+"&"+
-                 "redirect_uri="+redirect_uri+"&"+
+                 "redirect_uri="+redirect_login+"&"+
                  "response_type=token";
-    window.location=loginUrl;
+    parent.location=loginUrl;
     */
-	
+
 	FB.login(function(response) {
 		if (response.authResponse) {
 			var uid = response.authResponse.userID;
-			FB.api('/me', function(response) {
-				parent.location = redirect_login+'?uid='+uid+'&name='+response.name;      			
+			FB.api('/me', function(response) {		
+				try {		
+				parent.location = redirect_login + '?uid=' + uid
+												 + '&name=' + response.name
+												 + '&email=' + response.email 
+												 + '&birthday=' + response.birthday
+												 + '&location=' + response.location.name;
+				} catch (e){
+					alert(e);
+				}					    			
     		});
 		} else {
-			alert('請安心授權給籃球幫，我們保證不會在非經你的同意下回傳訊息至Facebook');
+			alert('請安心授權給籃球幫，我們保證不會在非經您的同意下回傳訊息至Facebook');
 		}
 	}, {
 		scope : 'user_birthday,email,read_stream,user_location,user_work_history,user_photos,user_activities,user_likes'
